67fdb1a4789b83850d87ac9908a32cfdfe65c30d
shivaniindoria <shivani.indoria30@gmail.com>
2023-07-12 21:05:12 +0530

    update api views

--- api/views.py
+++ api/views.py
@@ -4,15 +4,12 @@ from api.models import item
 
 class DumpItApi(APIView):
     def get(self,request):
-        items = [
-            'apple',
-            'grapes',
-            'mango'
-        ]
+        items = item.objects.all()
         response_data = {"datas":items}
         return Response(response_data,status=status.HTTP_200_OK)
 
     def post(self,request):
         name=request.data.get('name')
-        response_data = {'name':name}
+        item.objects.create(name=name)
+        response_data = {'response':'item created'}
         return Response(response_data,status=status.HTTP_200_OK)
