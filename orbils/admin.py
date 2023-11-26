16c5d11ea1e904e1416a1c53247676b6dd7a83a7
shivaniindoria <shivani.indoria30@gmail.com>
2023-11-26 13:45:02 +0530

    admin view

--- orbils/admin.py
+++ orbils/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 # Register your models here.
 from .models import Contact , Courses , CartItem
 admin.site.register(Contact)
-admin.site.register(Courses)
 admin.site.register(CartItem)
+
+class CoursesAdmin(admin.ModelAdmin):
+    list_display = ('coursename', 'shortdescription','display_image','category', 'original_price','discounted_price')
+    search_fields = ('coursename', 'shortdescription','category')
+    list_filter = ('category',)  # Add filters for 'created_at' field
+
+    # readonly_fields = ['display_image']
+    def display_image(self, obj):
+        if obj.courseimg:
+            return format_html('<img src="{}" style="max-height: 80px; max-width: 80px;border-radius:50%;" />', obj.courseimg.url)
+        else:
+            return 'No Image'
+
+    display_image.short_description = 'Image'
+
+admin.site.register(Courses, CoursesAdmin)
